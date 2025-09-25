adbf86f89ccc0f5a6ed91cbdc450c5f23f760d25
Naisthecreator <42411601+naisthecreator@users.noreply.github.com>
2025-09-25 01:30:49 +0000

    Add input and tasks state with form submit handler for todos

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,40 @@
 import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+  //input field state
+  const [input, setInput] = useState("")
+  //tasks state
 	const [tasks, setTasks] = useState([])
-	return(
-		<div class="container-fluid">
 
-      <div class="container">
-        <h1 class="h1 text-danger fw-light mb-2 text-center">todos</h1>
-        <div class="d-flex justify-content-center">
-          <div class="col-6">
+  //Handling form submit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(input.trim() === "");
+    setTasks(...tasks, input);
+    setInput("")
+
+  }
+	return(
+		<div className="container-fluid">
+
+      <div className="container">
+        <h1 className="h1 text-danger fw-light mb-2 text-center">todos</h1>
+        <div className="d-flex flex-column">
           <form>
-            <input type="text" placeholder="What needs to be done?" class="p-3 w-100"/>
+            <input type="text" placeholder="What needs to be done?" className="p-3 w-100"/>
           </form>
+              {tasks.map((task, indexOfTask) => (
+          <p key={indexOfTask} className="p-3 border w-100">{task}</p>
+        ))}
+
+          </div>
           </div>
           
+          
+          
         </div>
-      </div>
-    </div>
+          
 	)
 };
 
