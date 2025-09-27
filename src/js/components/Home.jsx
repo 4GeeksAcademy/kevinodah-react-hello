2de1e8a46d598770968580fe42c18537d97e8ff4
Naisthecreator <42411601+naisthecreator@users.noreply.github.com>
2025-09-27 16:32:40 +0000

    Added delete Icon show on hover

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,51 +1,47 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 const Home = () => {
-  //input field state
-  const [input, setInput] = useState("")
-  //tasks state
-	const [tasks, setTasks] = useState([])
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-  //Handling form submit
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if(input.trim() === "") return;
+    e.preventDefault();
+    if (input.trim() === "") return;
     setTasks([...tasks, input]);
-    setInput("")
+    setInput("");
+  };
 
-  }
-	return(
-		<div className="container-fluid">
-
+  return (
+    <div className="container-fluid">
       <div className="container">
         <h1 className="h1 text-danger fw-light mb-2 text-center">todos</h1>
         <div className="d-flex flex-column">
           <form onSubmit={handleSubmit}>
-            <input type="text" value={input}
-            onChange={e =>
-              setInput(e.target.value)
-            }
-             placeholder="What needs to be done?" className="p-3 w-100"/>
+            <input
+              type="text"
+              value={input}
+              onChange={e => setInput(e.target.value)}
+              placeholder="What needs to be done?"
+              className="p-3 w-100"
+            />
           </form>
-            {tasks.length === 0 ? (
-              <p className="text-center p-2">No tasks, add a task 📋</p>
-            ) : (
-              tasks.map(task => <p className="p-2 border mt-2">{task}
-              <span className="delete-icon"><i class="fa fa-trash"></i>
-</span></p>)
-            )}
 
-
-            
-          </div>
-          </div>
-          
-          
-          
+          {tasks.length === 0 ? (
+            <p className="text-center p-2">No tasks, add a task 📋</p>
+          ) : (
+            tasks.map((task, idx) => (
+              <div className="container d-flex justify-content-between align-items-center border p-3 mt-2" key={idx}>
+                <p className="mb-0">
+                  {task}
+                </p>
+                    <i className="delete-icon fa-solid fa-square-minus text-danger"></i>
+              </div>
+            ))
+          )}
         </div>
-          
-	)
+      </div>
+    </div>
+  );
 };
 
 export default Home;
